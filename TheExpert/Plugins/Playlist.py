6f9bd1c764403ecc45e3d0dc5e9de88c45171395
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-04-03 20:50:11 +0530

    Update Playlist.py

--- TheExpert/Plugins/Playlist.py
+++ TheExpert/Plugins/Playlist.py
@@ -3,14 +3,14 @@ from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,
                             KeyboardButton, Message, ReplyKeyboardMarkup,
                             ReplyKeyboardRemove)
 
-from Yukki import BOT_ID, BOT_USERNAME, MUSIC_BOT_NAME, SUDOERS, app, db_mem
-from Yukki.Database import (_get_playlists, delete_playlist, get_playlist,
+from TheExpert import BOT_ID, BOT_USERNAME, MUSIC_BOT_NAME, SUDOERS, app, db_mem
+from TheExpert.Database import (_get_playlists, delete_playlist, get_playlist,
                             get_playlist_names, save_playlist)
-from Yukki.Decorators.admins import AdminRightsCheck
-from Yukki.Decorators.assistant import AssistantAdd
-from Yukki.Decorators.checker import checker, checkerCB
-from Yukki.Decorators.permission import PermissionCheck
-from Yukki.Inline import (add_genre_markup, check_genre_markup, check_markup,
+from TheExpert.Decorators.admins import AdminRightsCheck
+from TheExpert.Decorators.assistant import AssistantAdd
+from TheExpert.Decorators.checker import checker, checkerCB
+from TheExpert.Decorators.permission import PermissionCheck
+from TheExpert.Inline import (add_genre_markup, check_genre_markup, check_markup,
                           delete_playlist_markuup, download_markup,
                           others_markup, play_genre_playlist, playlist_markup,
                           third_playlist_markup)
