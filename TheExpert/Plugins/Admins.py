82d211b508a2e26766e8771b3f1e3c9be16626f6
Mr_Invisible_Boy <101985120+MR-INVISIBLEBOY@users.noreply.github.com>
2022-04-03 20:30:17 +0530

    Update Admins.py

--- TheExpert/Plugins/Admins.py
+++ TheExpert/Plugins/Admins.py
@@ -9,26 +9,26 @@ from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineKeyboardMarkup, KeyboardButton, Message,
                             ReplyKeyboardMarkup, ReplyKeyboardRemove)
 
-from Yukki import BOT_USERNAME, MUSIC_BOT_NAME, app, db_mem
-from Yukki.Core.PyTgCalls import Queues
-from Yukki.Core.PyTgCalls.Converter import convert
-from Yukki.Core.PyTgCalls.Downloader import download
-from Yukki.Core.PyTgCalls.Yukki import (pause_stream, resume_stream, skip_video_stream,
+from TheExpert import BOT_USERNAME, MUSIC_BOT_NAME, app, db_mem
+from TheExpert.Core.PyTgCalls import Queues
+from TheExpert.Core.PyTgCalls.Converter import convert
+from TheExpert.Core.PyTgCalls.Downloader import download
+from TheExpert.Core.PyTgCalls.Yukki import (pause_stream, resume_stream, skip_video_stream,
                                         skip_stream, stop_stream)
-from Yukki.Database import (is_active_chat, is_music_playing, music_off, remove_active_video_chat,
+from TheExpert.Database import (is_active_chat, is_music_playing, music_off, remove_active_video_chat,
                             music_on, remove_active_chat)
-from Yukki.Decorators.admins import AdminRightsCheck
-from Yukki.Decorators.checker import checker, checkerCB
-from Yukki.Inline import audio_markup, primary_markup, secondary_markup2
-from Yukki.Utilities.changers import time_to_seconds
-from Yukki.Utilities.chat import specialfont_to_normal
-from Yukki.Utilities.theme import check_theme
-from Yukki.Utilities.thumbnails import gen_thumb
-from Yukki.Utilities.timer import start_timer
-from Yukki.Utilities.youtube import get_yt_info_id
+from TheExpert.Decorators.admins import AdminRightsCheck
+from TheExpert.Decorators.checker import checker, checkerCB
+from TheExpert.Inline import audio_markup, primary_markup, secondary_markup2
+from TheExpert.Utilities.changers import time_to_seconds
+from TheExpert.Utilities.chat import specialfont_to_normal
+from TheExpert.Utilities.theme import check_theme
+from TheExpert.Utilities.thumbnails import gen_thumb
+from TheExpert.Utilities.timer import start_timer
+from TheExpert.Utilities.youtube import get_yt_info_id
 
 
-from Yukki.Utilities.youtube import get_m3u8
+from TheExpert.Utilities.youtube import get_m3u8
 
 loop = asyncio.get_event_loop()
 
